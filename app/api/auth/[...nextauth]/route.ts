e9ea764a72544730de48b1bc7f5e4b85d78887c8
Xanthium7 <akshhaykmurali@gmail.com>
2026-06-22 00:14:24 +0530

    feat: implement NextAuth with Google provider and extend session types for user ID support

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,49 @@ const handler = NextAuth({
   ],
   pages: {
     signIn: "/auth/signin",
-  }
+  },
+  callbacks: {
+    async signIn({user, account, profile}){
+      if (account?.provider === "google") {
+        try{
+          // const existingUser = await db.user.findUnique({ 
+          //   where: { email: user.email } 
+          // });
+
+          // if (!existingUser) {
+          //   await db.user.create({
+          //     data: {
+          //       email: profile.email,
+          //       name: profile.name,
+          //       image: profile.picture,
+          //     }
+            // });
+            return true;
+          }
+         catch (error) {
+          console.log("Error in signin callback", error);
+          return false;
+        }
+      }
+      return true;
+    },
+
+    async jwt({token, user, trigger}){
+
+      if(user){
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    }
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+
 })
 
 export { handler as GET, handler as POST } 
