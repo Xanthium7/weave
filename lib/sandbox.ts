ef8c84fdcc75fdcab595022de1a53b617bcafc89
Xanthium7 <akshhaykmurali@gmail.com>
2026-06-28 23:33:43 +0530

    feat: implement sandbox creation and project bootstrapping utilities using E2B code interpreter

---
+++ lib/sandbox.ts
@@ -0,0 +1,36 @@
+import Sandbox from "@e2b/code-interpreter";
+
+export async function createSandbox() {
+  return await Sandbox.create({
+    apiKey: process.env.E2B_API_KEY!,
+    timeoutMs: 10 * 60 * 1000, // 10 mins
+  });
+}
+
+// to restore code, install deps, and strat dev server
+export async function bootstrapProject(
+  sandbox: Sandbox,
+  projectDir: string = "home/user/LovableProject",
+) {
+  await sandbox.commands.run(`cd ${projectDir} && npm install `, {
+    timeoutMs: 5 * 60 * 1000, // 5 mins
+  });
+
+  await sandbox.commands.run(
+    `cd ${projectDir} && npm run dev -- --host 0.0.0.0`,
+    {
+      background: true,
+    },
+  );
+
+  // give server time to start 
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  try {
+    const host = sandbox.getHost(3000);
+    return `https://${host}`;
+  } catch (e) {
+    console.error("Error getting host", e);
+    return null;
+  }
+}
